4460a7b72225247f8d1ee8513aa7ee9d1b113d82
ranjithcodein <githubaccnt4546@gmail.com>
2026-02-19 15:16:43 +0530

    update

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var typed= new Typed(".text",{
-    strings : ["Attacker","Web Developer"],
+    strings : [,"Attacker","Web Developer"],
     typeSpeed:100,
     backSpeed:100,
     backDelay:1000,
